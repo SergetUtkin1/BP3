0638e9925d56ae8a8f9211fdefa565f11e1b46bd
Sergey Utkin <74918383+SergetUtkin1@users.noreply.github.com>
2022-04-16 02:15:27 +0300

    Add files via upload

---
+++ BP3.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iomanip>
+
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 68; i++)
+		cout << "-";
+
+	cout << "\n : " << setw(3) << "x" << " : ";
+	cout << setprecision(3) << setw(7) << right << "x,rad" << " : ";
+	cout << setw(7) << right << "cos(x)" << " : ";
+	cout << setw(17) << right << "cos(x) + sin(x)" << " : ";
+	cout << setw(17) << right << "cos(sin(x))" << " : " << endl;
+	
+	for (int i = 0; i < 68; i++)
+		cout << "-";
+
+	for (int x = 0; x < 91; x+=5)
+	{
+		double radx = x * (M_PI / 180);
+		double cosinus = x == 90 ? 1 : cos(radx);
+		double sinus = x == 90 ? 0 : sin(radx);
+
+		cout << "\n : " << setw(3) << x << " : ";
+		cout << setprecision(3) << setw(7) << right << radx << " : ";
+		cout << setprecision(4) << setw(7) << right << cosinus << " : ";
+		cout << setw(17) << right << cosinus + sinus << " : ";
+		cout << setw(17) << right << cos(sinus) << " : ";
+	}
+	cout << endl;
+	for (int i = 0; i < 68; i++)
+		cout << "-";
+}
